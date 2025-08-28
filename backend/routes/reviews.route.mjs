9086b77f639f0e68d519b890f12a02c2904e9f62
Agustin <agubarbero22@gmail.com>
2025-08-27 22:49:12 -0300

    updates

--- backend/routes/reviews.route.mjs
+++ backend/routes/reviews.route.mjs
@@ -1,18 +1,17 @@
 import { Router } from "express";
 import { addReview } from "../controllers/reviews.controller.mjs";
-import rateLimit from "express-rate-limit";
 
-// Apply a rate limiter to prevent spam
+import rateLimit from "express-rate-limit";
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 10, // Limit each IP to 10 requests per window
+  windowMs: 10 * 60 * 1000,
+  limit: 10,
   standardHeaders: true,
   legacyHeaders: false,
+  ipv6Subnet: 56,
 });
 
 const reviewsRouter = Router();
 
-// Route for submitting a new review
 reviewsRouter.post("/", limiter, addReview);
 
 export default reviewsRouter;
