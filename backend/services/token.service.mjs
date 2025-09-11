e7486b2c052fe3bb7f7258ad3a6ef0888e788021
patricioscelso <patricioscelso@gmail.com>
2025-09-11 16:23:13 -0300

    fix verify-token

--- backend/services/token.service.mjs
+++ backend/services/token.service.mjs
@@ -1,6 +1,7 @@
 export const verifyToken = (token) => {
   return new Promise((resolve, reject) => {
-    const secretToken = process.env.REVIEWS_VIEW_TOKEN;
+    const secretToken = process.env.TURSO_DB_TOKEN;
+    console.log(process.env.TURSO_DB_TOKEN)
     if (token === secretToken) {
       resolve(true);
     } else {
